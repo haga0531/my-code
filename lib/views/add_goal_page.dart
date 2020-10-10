58d67dd2a37ae1283267bcb57a8f1dd3e2e0280a
haga0531 <bagy531hiro@gmail.com>
2020-10-10 21:02:53 +0900

    firestoreに保存

--- lib/views/add_goal_page.dart
+++ lib/views/add_goal_page.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:intl/intl.dart';
 import 'package:mycode/model/goal.dart';
 import 'package:mycode/views/add_sub_goal.dart';
+import 'package:mycode/views/home_page.dart';
 import 'package:mycode/views/sub_goal_list.dart';
 
 class AddGoalPage extends StatefulWidget {
@@ -14,6 +17,7 @@ class _AddGoalPageState extends State<AddGoalPage> {
   List<Goal> _goalLists = [];
   var _pickedDate = '締め切り';
   final mainGoalInputController = TextEditingController();
+  String _error;
 
   void _addNewGoal(String title, String date) {
     final newGoal = Goal(title: title, date: date);
@@ -22,11 +26,25 @@ class _AddGoalPageState extends State<AddGoalPage> {
     });
   }
 
-  void submit() {
-    print(_goalLists[0].title);
+  submit() async {
+    FirebaseUser currentUser = await FirebaseAuth.instance.currentUser();
+    DocumentReference goals = Firestore.instance.collection('goals').document();
 
-    print(_pickedDate);
-    print(mainGoalInputController.text);
+    final subGoalLists = [];
+    for (var i = 0; i < _goalLists.length; i++) {
+      subGoalLists.add({
+        'subGoalTitle': _goalLists[i].title,
+        'subGoalDeadline': _goalLists[i].date,
+      });
+    }
+    // todo mainDeadlineよりsubDeadlineの方が遅かったらアラートかける
+    goals.setData({
+      'userId': currentUser.uid,
+      'mainGoalTitle': mainGoalInputController.text,
+      'mainGoalDeadline':
+          DateFormat('yyyy-MM-dd').format(DateTime.parse(_pickedDate)),
+      'subGoalLists': subGoalLists,
+    });
   }
 
   Future<void> _selectDate(BuildContext context) async {
@@ -74,6 +92,40 @@ class _AddGoalPageState extends State<AddGoalPage> {
     );
   }
 
+  Widget showAlert() {
+    if (_error != null) {
+      return Container(
+        color: Colors.amberAccent,
+        width: double.infinity,
+        padding: EdgeInsets.all(8),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: Icon(Icons.error_outline),
+            ),
+            Expanded(
+                child: Text(
+              _error,
+              maxLines: 3,
+            )),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    setState(() {
+                      _error = null;
+                    });
+                  }),
+            )
+          ],
+        ),
+      );
+    }
+    return SizedBox();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,6 +140,7 @@ class _AddGoalPageState extends State<AddGoalPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
+              showAlert(),
               Container(
                 child: _buildMainGoal(),
               ),
@@ -102,7 +155,20 @@ class _AddGoalPageState extends State<AddGoalPage> {
                 height: 30,
               ),
               RaisedButton(
-                onPressed: submit,
+                onPressed: () async {
+                  if (mainGoalInputController.text == '' ||
+                      _pickedDate == '締め切り') {
+                    setState(() {
+                      _error = 'ゴールと締め切りを決めてください';
+                    });
+                    return;
+                  }
+                  submit().then((success) {
+                    mainGoalInputController.clear();
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomePage()));
+                  });
+                },
                 child: Text(
                   '保存する',
                   style: TextStyle(color: Colors.white),
